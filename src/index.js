f46b9611e930294d789bbf4fc30383a69a1b9567
Crystal Wang <crystalmaro@gmail.com>
2019-08-19 21:17:51 +0800

    wip ninga#9

--- src/index.js
+++ src/index.js
@@ -1,27 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const todos = [
-  {
-    task: 'make react tutorial',
-    isCompleted: false
-  },
-  {
-    task: 'eat dinner',
-    isCompleted: true
-  }
-]
+// const todos = [
+//   {
+//     task: 'make react tutorial',
+//     isCompleted: false
+//   },
+//   {
+//     task: 'eat dinner',
+//     isCompleted: true
+//   }
+// ]
 
 class App extends React.Component {
+  state = {
+    name: 'Ryu',
+    age: 30
+  }
   render() {
     return (
-      <div>
-          <h1>react todo app 8</h1>
+      <div className='app-content'>
+        <h1>ninja</h1>
+        <p>My name is: { this.state.name } and I am { this.state.age }</p>
+        <form>
+          <input type="text" />
+          <button>Submit</button>
+        </form>
       </div>
     )
   }
 }
 ReactDOM.render(<App />, document.querySelector('#app'));
+
+// var destination = document.querySelector('#app');
+// ReactDOM.render(
+//   <div>
+//     <p>hi indian</p>
+//   </div>,
+//   destination
+// );
 
 let msg = "hi again from entry";
 console.log(msg);
