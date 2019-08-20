6a9e1b5debf23444d8ad36e0c4698fae2418d117
Crystal Wang <crystalmaro@gmail.com>
2019-08-20 18:24:34 +0800

    finish tutorial with Ben Awad

---
+++ src/components/Todo.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default props => (
+  <div style={{display: 'flex'}}>
+    <div 
+      style={{
+          textDecoration: props.x.complete ? 'line-through' : '',
+      }}
+      onClick={props.toggleComplete }
+    >
+      {props.x.text}
+    </div>
+    <button 
+      onClick={props.handleDelete}
+    >
+      X
+    </button>
+  </div>
+);
+
+
+// const Todo = (props) => {
+//   <div 
+//   style={{
+//       textDecoration: props.x.complete ? 'line-through' : ''
+//   }}
+//   onClick={props.toggleComplete }
+// >
+//   {props.x.text}
+// </div>
+// }
+
+// export default Todo
+
