74806f9d01ecba9c8ba7f5f0b67679609e58ffe5
Crystal Wang <crystalmaro@gmail.com>
2019-08-23 09:13:12 +0800

    wip

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ class App extends Component {
         <div className="App">
           <Navbar />
           <Tick />
+          <Tick />
           <Route exact path='/' component={Home}/>
           <Route path='/about' component={About} />
           <Route path='/contact' component={Contact} />
---
+++ src/components/Tick.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+
+class Tick extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      date: new Date(),
+      count: 0,
+      text: 'hhh'
+    }
+  }
+
+  componentDidMount(){
+    this.timer = setInterval(() => {
+      this.tick()
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+
+  // method. can also add it directly into the componentDidMount()
+  tick(){
+    this.setState({
+      date: new Date(),
+      count: this.state.count+2
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div>{this.state.count} {this.state.text} {this.state.date.toLocaleTimeString()}</div>
+      </div>
+    )
+  }
+}
+
+
+
+export default Tick
